a4b3fd731aae8c5e91bc1474fb8d6e6fa344b778
Al4exander <tarasenko1794@gmail.com>
2015-11-16 14:17:33 +0200

    FirstCommit

---
+++ Task_2/Controllers/myController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_2.Views.my;
+
+namespace Task_2.Controllers
+{
+    public class myController : Controller
+    {
+        // GET: my
+        public ActionResult task131()
+        {
+            List<string> str = new List<string>();
+
+            str.Add("<li>Строка№1</li>");
+            str.Add("<li>Строка№2</li>");
+            str.Add("<li>Строка№3</li>");
+
+            return new AddHTML(str);
+        }
+
+        public ActionResult task132()
+        {
+            List<string> str = new List<string>();
+
+            str.Add("<li>1</li>");
+            str.Add("<li>Два</li>");
+            str.Add("<li>Three</li>");
+            str.Add("<li>Чотири</li>");
+
+            return new AddHTML(str);
+        }
+
+        public ActionResult task133()
+        {
+            List<string> str = new List<string>();
+
+            str.Add("<li>Кот</li>");
+            str.Add("<li>Собака</li>");
+
+            return new AddHTML(str);
+        }
+    }
+}
---
+++ Task_2/Views/my/AddHTML.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Task_2.Views.my
+{
+    public class AddHTML : ActionResult
+    {
+        private List<string> forHtml;
+
+        public AddHTML(List<string> forHtml)
+        {
+            this.forHtml = forHtml;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+         fullHtmlCode += "<title>Главная страница</title>";
+         fullHtmlCode += "<meta charset = utf-8 />";
+         fullHtmlCode += "</head> <ol>";
+            foreach(string str in forHtml)
+            {
+               fullHtmlCode += str;
+            }
+         fullHtmlCode += "</ol></html>";
+         context.HttpContext.Response.Write(fullHtmlCode); 
+        } 
+    } 
+}
+
+        
